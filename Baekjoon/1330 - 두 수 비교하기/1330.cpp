e2d8f9fa90323fd4981e9e10fd52f505ed76d9df
m1n5eo <skjd1234@naver.com>
2022-01-07 10:02:28 +0900

    Baekjoon 1330 C++

--- Baekjoon/1330 - 두 수 비교하기/1330.cpp
+++ Baekjoon/1330 - 두 수 비교하기/1330.cpp
@@ -1,10 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
     int a, b;
-    scanf("%d %d", &a, &b);
-    if(a < b) printf("<");
-    else if(a > b) printf(">");
-    else printf("==");
+    cin >> a >> b;
+    if(a < b) cout << "<";
+    else if(a > b) cout << ">";
+    else cout << "==";
 }
