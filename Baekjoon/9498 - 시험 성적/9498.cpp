eb541cdb39c74db03f459e348ebc8b2f32706aeb
m1n5eo <skjd1234@naver.com>
2022-01-11 22:07:12 +0900

    Baekjoon 9498 C++

--- Baekjoon/9498 - 시험 성적/9498.cpp
+++ Baekjoon/9498 - 시험 성적/9498.cpp
@@ -1,12 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
     int n;
-    scanf("%d", &n);
-    if(n >= 90) printf("A");
-    else if(n >= 80) printf("B");
-    else if(n >= 70) printf("C");
-    else if(n >= 60) printf("D");
-    else printf("F");
+    cin >> n;
+    if(n >= 90) cout << "A";
+    else if(n >= 80) cout << "B";
+    else if(n >= 70) cout << "C";
+    else if(n >= 60) cout << "D";
+    else cout << "F";
 }
