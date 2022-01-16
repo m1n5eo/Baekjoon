943d52e58edf2bfb16c33c3659d2ad0b40efca89
m1n5eo <skjd1234@naver.com>
2022-01-16 22:19:48 +0900

    Baekjoon 2884 C++

--- Baekjoon/2884 - 알람 시계/2884.cpp
+++ Baekjoon/2884 - 알람 시계/2884.cpp
@@ -1,12 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
     int h, m, sum = 0; // 24*60 = 1440
-    scanf("%d %d", &h, &m);
+    cin >> h >> m;
     sum = h*60 + m;
     if(sum < 45) sum = 1440-(45-sum);
     else sum = sum-45;
 
-    printf("%d %d", sum/60, sum%60);
+    cout << sum/60 << " " << sum%60;
 }
