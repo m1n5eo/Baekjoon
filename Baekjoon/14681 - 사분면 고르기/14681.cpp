6b9aed2b8c34799cb5824bf806fddda22596728b
m1n5eo <skjd1234@naver.com>
2022-01-16 22:14:23 +0900

    Baekjoon 14681 C++

--- Baekjoon/14681 - 사분면 고르기/14681.cpp
+++ Baekjoon/14681 - 사분면 고르기/14681.cpp
@@ -1,11 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
     int x, y;
-    scanf("%d %d", &x, &y);
-    if(x > 0 && y > 0) printf("1");
-    else if(x < 0 && y > 0) printf("2");
-    else if(x < 0 && y < 0) printf("3");
-    else if(x > 0 && y < 0) printf("4");
+    cin >> x >> y;
+    if(x > 0 && y > 0) cout << "1";
+    else if(x < 0 && y > 0) cout << "2";
+    else if(x < 0 && y < 0) cout << "3";
+    else if(x > 0 && y < 0) cout << "4";
 }
