46c0babb23813f98000a5026dc01eaa3d562bc71
m1n5eo <skjd1234@naver.com>
2023-11-21 01:52:25 +0900

    Baekjoon 10807 C++

--- Baekjoon/10807 - 개수 세기/10807.cpp
+++ Baekjoon/10807 - 개수 세기/10807.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-int n, m, cnt[111];
+int n, m, cnt[555];
 
 int main() {
     cin >> n;
     for(int i = 0; i < n; i++) {
         cin >> m;
-        cnt[m]++;
+        cnt[m+200]++;
     }
     cin >> m;
-    cout << cnt[m];
+    cout << cnt[m+200];
 }
