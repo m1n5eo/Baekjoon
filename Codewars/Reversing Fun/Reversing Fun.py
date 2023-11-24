a8fbfc7941fa7fe8a8cd4a0c1b5c213a94050aba
m1n5eo <skjd1234@naver.com>
2023-11-25 04:39:08 +0900

    Codewars Reversing Fun Python

--- Codewars/Reversing Fun/Reversing Fun.py
+++ Codewars/Reversing Fun/Reversing Fun.py
@@ -1,7 +1,7 @@
 def reverse_fun(string):
     ans = ""
     for i in range(0, len(string)//2):
-        ans = ans + string[len(string)-i-1] + string[i]
+        ans = ans + string[len(string)-1-i] + string[i]
     if len(string)%2 == 1:
         ans = ans + string[len(string)//2]
     return ans
