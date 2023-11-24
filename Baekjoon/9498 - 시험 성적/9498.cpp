c975c360b417598e05f172ca0139fd0d18e183ce
m1n5eo <skjd1234@naver.com>
2023-11-25 02:24:37 +0900

    Baekjoon 9498 C++

--- Baekjoon/9498 - 시험 성적/9498.cpp
+++ Baekjoon/9498 - 시험 성적/9498.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    if(n >= 90) cout << "A";
-    else if(n >= 80) cout << "B";
-    else if(n >= 70) cout << "C";
-    else if(n >= 60) cout << "D";
+    int score;
+    cin >> score;
+    if(score >= 90) cout << "A";
+    else if(score >= 80) cout << "B";
+    else if(score >= 70) cout << "C";
+    else if(score >= 60) cout << "D";
     else cout << "F";
 }
