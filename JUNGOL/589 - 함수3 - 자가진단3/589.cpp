52e45944db4c2d53ede20c48920bcba6fb84733a
m1n5eo <skjd1234@naver.com>
2023-12-04 14:36:21 +0900

    JUNGOL 589 C++

---
+++ JUNGOL/589 - 함수3 - 자가진단3/589.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void print(int num, int res) {
+    if(num == 0) {
+        cout << res;
+        return;
+    }
+    print(num-1, res+num);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    print(n, 0);
+}
