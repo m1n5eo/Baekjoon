773e4ddc549cafffe86c6aebe6df1841f7a6d1e7
m1n5eo <skjd1234@naver.com>
2023-12-12 21:15:02 +0900

    JUNGOL 1707 C++

---
+++ JUNGOL/1707 - 달팽이사각형/1707.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int arr[111][111];
+
+int main() {
+    int n;
+    cin >> n;
+
+    int i = 0, j = 0, sw = 0, cnt = 1;
+    while(true) {
+        arr[i][j] = cnt;
+        cnt += 1;
+
+        if(cnt-1 == n*n) break;
+
+        if(sw == 0) {
+            if(j+1 == n || arr[i][j+1] != 0) {
+                i = i+1;
+                sw = 1;
+            }
+            else j = j+1;
+        }
+        else if(sw == 1) {
+            if(i+1 == n || arr[i+1][j] != 0) {
+                j = j-1;
+                sw = 2;
+            }
+            else i = i+1;
+        }
+        else if(sw == 2) {
+            if(j == 0 || arr[i][j-1] != 0) {
+                i = i-1;
+                sw = 3;
+            }
+            else j = j-1;
+        }
+        else if(sw == 3) {
+            if(i == 0 || arr[i-1][j] != 0) {
+                j = j+1;
+                sw = 0;
+            }
+            else i = i-1;
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
