010a51ecf49d39d8926332cdeff46c27f4706c15
m1n5eo <skjd1234@naver.com>
2023-12-13 14:55:40 +0900

    JUNGOL 186 C++

---
+++ JUNGOL/186 - 문자열1 - 형성평가5/186.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    if(s1.size() < s2.size()) cout << s2.size();
+    else cout << s1.size();
+}
