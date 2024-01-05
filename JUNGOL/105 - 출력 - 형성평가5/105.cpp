dcaf5faa19114d199818b91a411cd31493792817
m1n5eo <skjd1234@naver.com>
2024-01-05 14:55:11 +0900

    JUNGOL 105 C++

---
+++ JUNGOL/105 - 출력 - 형성평가5/105.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cout.width(15); cout << right << "Seoul";
+	cout.width(15); cout << right << "10,312,545";
+	cout.width(15); cout << right << "+91,375" << "\n";
+	cout.width(15); cout << right << "Pusan";
+	cout.width(15); cout << right << "3,567,910";
+	cout.width(15); cout << right << "+5,868" << "\n";
+	cout.width(15); cout << right << "Incheon";
+	cout.width(15); cout << right << "2,758,296";
+	cout.width(15); cout << right << "+64,888" << "\n";
+	cout.width(15); cout << right << "Daegu";
+	cout.width(15); cout << right << "2,511,676";
+	cout.width(15); cout << right << "+17,230" << "\n";
+	cout.width(15); cout << right << "Gwangju";
+	cout.width(15); cout << right << "1,454,636";
+	cout.width(15); cout << right << "+29,774";
+}
