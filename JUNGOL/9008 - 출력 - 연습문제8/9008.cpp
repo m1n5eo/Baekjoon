11699f9d8cfb863fe9def5a23ac66ea1dfbee41b
m1n5eo <skjd1234@naver.com>
2024-01-05 14:26:35 +0900

    JUNGOL 9008 C++

---
+++ JUNGOL/9008 - 출력 - 연습문제8/9008.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << " subject score" << "\n";
+	cout << "==============" << "\n";
+	cout << "  korean    90" << "\n";
+	cout << " english   100" << "\n";
+	cout << "computer    80";
+}
