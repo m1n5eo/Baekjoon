90d27a246b585d81de7d03802bc56b73b0574b42
m1n5eo <skjd1234@naver.com>
2024-01-05 14:35:44 +0900

    JUNGOL 506 C++

---
+++ JUNGOL/506 - 출력 - 자가진단6/506.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cout << "My height" << "\n";
+	cout << 170 << "\n";
+	cout << "My weight" << "\n";
+	cout << showpoint << setprecision(8) << 68.6;
+}
