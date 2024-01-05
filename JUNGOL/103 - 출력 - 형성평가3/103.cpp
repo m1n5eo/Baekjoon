5189129fd00f85fb0bc1657c495204ae392f587e
m1n5eo <skjd1234@naver.com>
2024-01-05 14:47:59 +0900

    JUNGOL 103 C++

---
+++ JUNGOL/103 - 출력 - 형성평가3/103.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "TTTTTTTTTT" << "\n";
+	cout << "TTTTTTTTTT" << "\n";
+	cout << "    TT" << "\n";
+	cout << "    TT" << "\n";
+	cout << "    TT";
+}
