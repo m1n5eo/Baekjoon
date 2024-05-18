afe6dabf35d13e06c62eb0612e8f628282cf59a9
m1n5eo <skjd1234@naver.com>
2024-05-18 10:56:07 +0900

    Baekjoon 2696 C++

--- Baekjoon/2696 - 중앙값 구하기/2696.cpp
+++ Baekjoon/2696 - 중앙값 구하기/2696.cpp
@@ -1,33 +1,51 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-priority_queue<int>minq;
-priority_queue<int>maxq;
+int t, n;
+long int temp;
+
+priority_queue<long int> minq;
+priority_queue<long int> maxq;
 
 int main() {
-    int t, n, k;
-    scanf("%d", &t);
+    // cin.tie(NULL); cout.tie(NULL);
+    // ios::sync_with_stdio(false);
+
+    cin >> t;
     for(int testcase = 0; testcase < t; testcase++) {
-        scanf("%d", &n);
-        printf("%d\n", n/2+1);
+        cin >> n;
+        cout << n/2+1 << "\n";
         for(int i = 0; i < n; i++) {
-            scanf("%d", &k);
-            if(minq.size() == 0) minq.push(k);
-            else if(maxq.size() == 0) maxq.push(-k);
-            else if(minq.size() == maxq.size()) minq.push(k);
-            else maxq.push(-k);
-            if(minq.size() != 0 && maxq.size() != 0 && minq.top() > -maxq.top()) {
-                int a = minq.top();
-                int b = -maxq.top();
+            cin >> temp;
+
+            int minq_size = minq.size();
+            int maxq_size = maxq.size();
+            
+            if(minq_size == 0) minq.push(temp);
+            else if(maxq_size == 0) maxq.push(-temp);
+            else if(minq_size == maxq_size) minq.push(temp);
+            else maxq.push(-temp);
+
+            if(i > 0 && minq.top() > -maxq.top()) {
+                long int a = minq.top();
+                long int b = -maxq.top();
+
                 minq.pop();
                 maxq.pop();
+                
                 minq.push(b);
                 maxq.push(-a);
             }
-            if(i%2 == 0) printf("%d ", minq.top());
+
+            if(i%2 == 0) cout << minq.top() << " ";
+            else if(i%20 == 19) cout << "\n";
         }
-        printf("\n");
-        while(!minq.empty()) minq.pop();
-        while(!maxq.empty()) maxq.pop();
+        cout << "\n";
+
+        while(minq.size()) minq.pop();
+        while(maxq.size()) maxq.pop();
     }
+
+    return 0;
 }
