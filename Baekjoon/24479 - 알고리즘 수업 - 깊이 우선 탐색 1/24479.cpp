2e5a5f7a88aef30250d7c182fdf4735af5169cf4
m1n5eo <skjd1234@naver.com>
2024-06-16 04:51:21 +0900

    Baekjoon 24479 C++

---
+++ Baekjoon/24479 - 알고리즘 수업 - 깊이 우선 탐색 1/24479.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define MAX 111111
+#define ulli unsigned long long int
+
+int n, m, r, cnt, result[MAX];
+bool visit[MAX];
+vector<int> node[MAX];
+
+void dfs(int x) {
+    if(visit[x] == true) return;
+
+    cnt += 1;
+    visit[x] = true;
+    result[x] = cnt;
+
+    int node_size = node[x].size();
+
+    for(int i = 0; i < node_size; i++) {
+        dfs(node[x][i]);
+    }
+
+    return;
+}
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> n >> m >> r;
+
+    for(int _ = 0; _ < m; _++) {
+        int a, b;
+        cin >> a >> b;
+
+        node[a].push_back(b);
+        node[b].push_back(a);
+    }
+
+    for(int i = 1; i <= n; i++) {
+        sort(node[i].begin(), node[i].end());
+    }
+
+    dfs(r);
+
+    for(int i = 1; i <= n; i++) {
+        cout << result[i] << "\n";
+    }
+}
