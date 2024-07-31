2e9cbb26467ce9e1267c9deaf0a88ad01aa7aedb
m1n5eo <skjd1234@naver.com>
2024-07-31 20:16:51 +0900

    Baekjoon 2357 C++

--- Baekjoon/2357 - 최솟값과 최댓값/2357.cpp
+++ Baekjoon/2357 - 최솟값과 최댓값/2357.cpp
@@ -1,45 +1,62 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-long long int n, m, k, a, b, sw;
-long long int arr[1234567], min_tree[4*1234567], max_tree[4*1234567];
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define INF 2100000000
+#define MAX 111111
 
-long long int min_init(long long int start, int long long end, long long int node) {
-    if(start == end) return min_tree[node] = arr[start];
-    long long int mid = (start+end)/2;
+int n, m;
+vector<int> v(MAX, 0), min_tree(MAX*4, INF), max_tree(MAX*4, -INF);
+
+int min_init(int start, int end, int node) {
+    if(start == end) return min_tree[node] = v[start];
+
+    int mid = (start+end)/2;
     return min_tree[node] = min(min_init(start, mid, node*2), min_init(mid+1, end, node*2+1));
 }
 
-long long int max_init(long long int start, int long long end, long long int node) {
-    if(start == end) return max_tree[node] = arr[start];
-    long long int mid = (start+end)/2;
+int max_init(int start, int end, int node) {
+    if(start == end) return max_tree[node] = v[start];
+
+    int mid = (start+end)/2;
     return max_tree[node] = max(max_init(start, mid, node*2), max_init(mid+1, end, node*2+1));
 }
 
-long long int min_segment(long long int start, long long int end, long long int node, long long int left, long long int right) {
-    if(left > end || right < start) return 2100000000;
-    if(left <= start && end <= right) return min_tree[node];
-    long long int mid = (start+end)/2;
+int min_segment(int start, int end, int node, int left, int right) {
+    if(left > end || right < start) return INF;
+    else if(left <= start && end <= right) return min_tree[node];
+
+    int mid = (start+end)/2;
     return min(min_segment(start, mid, node*2, left, right), min_segment(mid+1, end, node*2+1, left, right));
 }
 
-long long int max_segment(long long int start, long long int end, long long int node, long long int left, long long int right) {
-    if(left > end || right < start) return 0;
-    if(left <= start && end <= right) return max_tree[node];
-    long long int mid = (start+end)/2;
+int max_segment(int start, int end, int node, int left, int right) {
+    if(left > end || right < start) return -INF;
+    else if(left <= start && end <= right) return max_tree[node];
+
+    int mid = (start+end)/2;
     return max(max_segment(start, mid, node*2, left, right), max_segment(mid+1, end, node*2+1, left, right));
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    FASTIO
 
     cin >> n >> m;
-    for(long long int i = 0; i < n; i++) cin >> arr[i];
+
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
     min_init(0, n-1, 1);
     max_init(0, n-1, 1);
-    for(long long int i = 0; i < m; i++) {
+
+    for(int i = 0; i < m; i++) {
+        int a, b;
+        
         cin >> a >> b;
-        cout << min_segment(0, n-1, 1, a-1, b-1) << ' ' << max_segment(0, n-1, 1, a-1, b-1) << '\n';
+
+        cout << min_segment(0, n-1, 1, a-1, b-1) << " " << max_segment(0, n-1, 1, a-1, b-1) << "\n";
     }
 }
