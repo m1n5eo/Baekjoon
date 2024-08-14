bcd889f49fb1bc5176213e8b7876f5e074c2a9b8
m1n5eo <skjd1234@naver.com>
2024-08-15 04:38:30 +0900

    Baekjoon 5341 Python

---
+++ Baekjoon/5341 - Pyramids/5341.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    n = int(input())
+    result = 0
+
+    if n == 0:
+        break
+
+    for i in range(1, n+1):
+        result += i
+    
+    print(result)
