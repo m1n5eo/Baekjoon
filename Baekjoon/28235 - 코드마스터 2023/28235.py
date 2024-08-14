4fcb1381f9deb5f587a9bbb11423218f5d10cf3d
m1n5eo <skjd1234@naver.com>
2024-08-14 12:32:09 +0900

    Baekjoon 28235 Python

---
+++ Baekjoon/28235 - 코드마스터 2023/28235.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+str = input()[:-1]
+
+if str == "SONGDO":
+    print("HIGHSCHOOL")
+elif str == "CODE":
+    print("MASTER")
+elif str == "2023":
+    print("0611")
+elif str == "ALGORITHM":
+    print("CONTEST")
