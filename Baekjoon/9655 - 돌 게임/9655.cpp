26dbb7cbcc197a15185ecb87924a528eb98a2f2a
m1n5eo <skjd1234@naver.com>
2024-08-24 00:37:52 +0900

    Baekjoon 9655 C++

---
+++ Baekjoon/9655 - 돌 게임/9655.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+int n;
+
+int main() {
+    FASTIO
+
+    cin >> n;
+
+    if(n%2 == 1) cout << "SK";
+    else cout << "CY";
+}
