fb3d6bcbae606e0f78e2c022ec179264868728a9
m1n5eo <skjd1234@naver.com>
2024-09-19 01:05:36 +0900

    Baekjoon 10101 C++

--- Baekjoon/10101 - 삼각형 외우기/10101.cpp
+++ Baekjoon/10101 - 삼각형 외우기/10101.cpp
@@ -1,11 +1,15 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 
 int main() {
     int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    if(a == 60 && b == 60 && c == 60) printf("Equilateral");
-    else if(a+b+c == 180 && (a == b || b == c || c == a)) printf("Isosceles");
-    else if(a+b+c == 180 && !(a == b || b == c || c == a)) printf("Scalene");
-    else printf("Error");
+
+    cin >> a >> b >> c;
+
+    if(a == 60 && b == 60 && c == 60) cout << "Equilateral";
+    else if(a+b+c == 180 && (a == b || b == c || c == a)) cout << "Isosceles";
+    else if(a+b+c == 180 && !(a == b || b == c || c == a)) cout << "Scalene";
+    else cout << "Error";
 }
