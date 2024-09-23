2871a615199df19b18e3e0385d6129b1a50e1217
m1n5eo <skjd1234@naver.com>
2024-09-23 22:09:07 +0900

    Update 1193.cpp

--- solution/01xxx/1193 - 분수찾기/1193.cpp
+++ solution/01xxx/1193 - 분수찾기/1193.cpp
@@ -1,18 +1,38 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 
 int main() {
+    FASTIO
+    
     int n, x = 1, y = 1, sw = 1, k = 1;
-    scanf("%d", &n);
-    while(1) {
+
+    cin >> n;
+
+    while(true) {
         if(k == n) break;
-        if(x == 1 || y == 1) {
+        else if(x == 1 || y == 1) {
             sw = 1-sw;
-            if(sw == 0) {y++;k++;}
-            else if(sw == 1) {x++;k++;}
+            k += 1;
+
+            if(sw == 0) y += 1;
+            else if(sw == 1) x += 1;
         }
+        
         if(k == n) break;
-        if(sw == 0) {x++;y--;k++;}
-        else if(sw == 1) {x--;y++;k++;}
+        else if(sw == 0) {
+            x += 1;
+            y -= 1;
+        }
+        else if(sw == 1) {
+            x -= 1;
+            y += 1;
+        }
+
+        k += 1;
     }
-    printf("%d/%d", x, y);
+
+    cout << x << "/" << y;
 }
