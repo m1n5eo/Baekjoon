3b8974a3866f13a2c19802e406052446b89c68d1
m1n5eo <skjd1234@naver.com>
2024-09-23 22:20:21 +0900

    Update 1212.cpp

--- solution/01xxx/1212 - 8진수 2진수/1212.cpp
+++ solution/01xxx/1212 - 8진수 2진수/1212.cpp
@@ -1,11 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
 int main() {
-    int n[8] = {0, 1, 10, 11, 100, 101, 110, 111};
-    char scan[333336];
-    scanf("%s", scan);
-    printf("%d", n[scan[0]-'0']);
-    int len = strlen(scan);
-    for(int i = 1; i < len; i++) printf("%03d", n[scan[i]-'0']);
+    FASTIO
+
+    vector<string> vec = {"000", "001", "010", "011", "100", "101", "110", "111"};
+    string scan;
+
+    cin >> scan;
+
+    int scan_size = scan.size();
+    
+    for(int i = 0; i < scan_size; i++) {
+        if(i != 0) cout << vec[scan[i]-'0'];
+        else {
+            if(scan[i]-'0' == 0) cout << "0";
+            else if(scan[i]-'0' == 1) cout << "1";
+            else if(scan[i]-'0' == 2) cout << "10";
+            else if(scan[i]-'0' == 3) cout << "11";
+            else cout << vec[scan[i]-'0'];
+        }
+    }
 }
