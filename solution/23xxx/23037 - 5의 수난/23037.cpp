f9a94337e78309cc9b31d3234e02449f8b8d0289
m1n5eo <skjd1234@naver.com>
2024-09-26 21:00:03 +0900

    Update 23037.cpp

--- solution/23xxx/23037 - 5의 수난/23037.cpp
+++ solution/23xxx/23037 - 5의 수난/23037.cpp
@@ -1,17 +1,30 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
+
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 
 int func(int num) {
     int rtn = 1;
-    for(int i = 0; i < 5; i++) rtn *= num;
+
+    for(int i = 0; i < 5; i++) {
+        rtn *= num;
+    }
+
     return rtn;
 }
 
 int main() {
-    int temp, sum = 0;
-    for(int i = 0; i < 5; i++) {
-        scanf("%1d", &temp);
-        sum += func(temp);
+    FASTIO
+
+    int sum = 0;
+    string num;
+
+    cin >> num;
+
+    for(auto nums : num) {
+        sum += func(nums-'0');
     }
-    printf("%d", sum);
+
+    cout << sum;
 }
