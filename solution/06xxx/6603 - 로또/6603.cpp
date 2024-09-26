15a9b497589c49b3dcbcba67756fceba3e50bff3
m1n5eo <skjd1234@naver.com>
2024-09-26 19:29:44 +0900

    Update 6603.cpp

--- solution/06xxx/6603 - 로또/6603.cpp
+++ solution/06xxx/6603 - 로또/6603.cpp
@@ -1,15 +1,43 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int n = 1, lotto[7], arr[14];
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 
-void f(int idx, int end_i) {
+int n = 1;
+vector<int> lotto(22, 0), v(22, 0);
+
+void f(int idx, int end) {
     if(idx == 6) {
-        for(int i = 0; i < 6; i++) printf("%d ", lotto[i]);
-        printf("\n");
+        for(int i = 0; i < 6; i++) {
+            cout << lotto[i] << " ";
+        }
+        cout << "\n";
+        return;
     }
-    for(int i = end_i+1; i < n; i++) {
-        lotto[idx] = arr[i];
+
+    for(int i = end+1; i < n; i++) {
+        lotto[idx] = v[i];
         f(idx+1, i);
+    }
+}
+
+int main() {
+    FASTIO
+
+    while(true) {
+        cin >> n;
+
+        if(n == 0) {
+            break;
+        }
+
+        for(int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+
+        f(0, -1);
+
+        cout << "\n";
     }
 }
