d50f681c9acc41eb9af3affbdba3b6eb11cb877f
m1n5eo <skjd1234@naver.com>
2024-09-26 18:56:11 +0900

    Update 4963.cpp

--- solution/04xxx/4963 - 섬의 개수/4963.cpp
+++ solution/04xxx/4963 - 섬의 개수/4963.cpp
@@ -1,48 +1,67 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
-int n, m, cnt, k;
-int arr[55][55];
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 
-void dfs(int x, int y) {
-    if(x < 0 || x > n-1 || y < 0 || y > m-1) return;
-    if(!(arr[x][y] == 1)) return;
-    //printf("%d %d \ ", x, y);
-    arr[x][y] = k;
-    dfs(x-1, y);
-    dfs(x+1, y);
-    dfs(x, y-1);
-    dfs(x, y+1);
-    dfs(x-1, y-1);
-    dfs(x-1, y+1);
-    dfs(x+1, y-1);
-    dfs(x+1, y+1);
-}
+struct coordinate {
+    int x, y;
+};
+
+int dx[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
 
 int main() {
-    scanf("%d %d", &m, &n);
-    while(n != 0 && m != 0) {
+    FASTIO
+
+    while(true) {
+        int n, m;
+
+        cin >> m >> n;
+
+        if(n == 0 && m == 0) {
+            break;
+        }
+
+        vector<vector<int>> map(n+1, vector<int>(m+1, 0));
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                scanf("%d", &arr[i][j]);
+                cin >> map[i][j];
             }
         }
-        k = 2;
+
+        int cnt = 0;
+        vector<vector<bool>> check(n+1, vector<bool>(m+1, false));
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(arr[i][j] == 1) {
-                    dfs(i, j);
-                    k++;
+                if(map[i][j] == 1 && check[i][j] == false) {
+                    queue<coordinate> bfs;
+                    bfs.push({i, j});
+                    check[i][j] = true;
+                    cnt += 1;
+                    
+                    while(bfs.size()) {
+                        coordinate now = bfs.front();
+                        bfs.pop();
+
+                        for(int k = 0; k < 8; k++) {
+                            coordinate next = {now.x+dx[k], now.y+dy[k]};
+
+                            if(0 <= next.x && next.x < n && 0 <= next.y && next.y < m) {
+                                if(map[next.x][next.y] == 1 && check[next.x][next.y] == false) {
+                                    bfs.push({next.x, next.y});
+                                    check[next.x][next.y] = true;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-        printf("%d\n", k-2);
-        /*for(int i = 0; i < n; i++) {
-            for(int j = 0; j< m;j++) {
-                printf("%d", arr[i][j]);
-            }
-            printf("\n");
-        }*/
-        scanf("%d %d", &m, &n);
+
+        cout << cnt << "\n";
     }
 }
