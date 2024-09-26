dbf2e24bcd3d4f08971d5898bb2c0c3bc14bc219
m1n5eo <skjd1234@naver.com>
2024-09-26 19:34:31 +0900

    Update 8370.cpp

--- solution/08xxx/8370 - Plane/8370.cpp
+++ solution/08xxx/8370 - Plane/8370.cpp
@@ -1,8 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
 int main() {
+    FASTIO
+
     int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    printf("%d", a*b+c*d);
+
+    cin >> a >> b >> c >> d;
+    cout << a*b+c*d;
 }
