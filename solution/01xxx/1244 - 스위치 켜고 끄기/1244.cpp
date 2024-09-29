ba474d95b2e110b16000040feb1ab8e5a30e2850
m1n5eo <skjd1234@naver.com>
2024-09-30 03:25:50 +0900

    Update 1244.cpp

--- solution/01xxx/1244 - 스위치 켜고 끄기/1244.cpp
+++ solution/01xxx/1244 - 스위치 켜고 끄기/1244.cpp
@@ -1,28 +1,59 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MAX 111
+
 int main() {
-    int n, switchValue[111], student, gen, num;
-    int startnum, endnum;
-    scanf("%d", &n);
-    for(int i = 1; i <= n; i++) scanf("%d", &switchValue[i]);
-    scanf("%d", &student);
-    for(int i = 0; i < student; i++) {
-        scanf("%d %d", &gen, &num);
-        if(gen == 1) {          // 남학생
-            for(int i = num; i <= n; i+=num) switchValue[i] = 1-switchValue[i];
-        }
-        else if(gen == 2) {     // 여학생
-            startnum = num, endnum = num;
-            while(startnum != 1 && endnum != n) {
-                if(switchValue[startnum-1] != switchValue[endnum+1]) break;
-                startnum--; endnum++;
+    FASTIO
+    
+    int n, m, temp, gen, num;
+    vector<bool> value(MAX, false);
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        cin >> temp;
+
+        if(temp == 0) value[i] = false;
+        else if(temp == 1) value[i] = true;
+    }
+
+    cin >> m;
+
+    for(int i = 0; i < m; i++) {
+        cin >> gen >> num;
+
+        if(gen == 1) {
+            for(int i = num; i <= n; i+=num) {
+                value[i] = !value[i];
             }
-            for(int k = startnum; k <= endnum; k++) switchValue[k] = 1-switchValue[k];
+        }
+        else if(gen == 2) {
+            int start = num, end = num;
+
+            while(start != 1 && end != n) {
+                if(value[start-1] != value[end+1]) {
+                    break;
+                }
+                
+                start -= 1;
+                end += 1;
+            }
+
+            for(int k = start; k <= end; k++) {
+                value[k] = !value[k];
+            }
         }
     }
+
     for(int i = 1; i <= n; i++) {
-        printf("%d ", switchValue[i]);
-        if(!(i%20)) printf("\n");
+        if(value[i] == true) cout << 1 << " ";
+        else if(value[i] == false) cout << 0 << " ";
+
+        if(i%20 == 0) {
+            cout << "\n";
+        }
     }
 }
