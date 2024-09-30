09607f2708dbb31e4eea8e97f3744a5f8ad8625d
m1n5eo <skjd1234@naver.com>
2024-09-30 19:36:44 +0900

    Update 1260.cpp

--- solution/01xxx/1260 - DFS와 BFS/1260.cpp
+++ solution/01xxx/1260 - DFS와 BFS/1260.cpp
@@ -1,45 +1,71 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
-int n, m, v, a, b;
-int arr[1001][1001], visit[1001];
-queue <int> q;
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MAX 1111
+
+int n, m, k, a, b;
+vector<vector<bool>> v(MAX, vector<bool>(MAX, 0));
+vector<bool> visit(MAX, false);
+queue<int> q;
 
 void bfs(int idx) {
-    visit[idx] = 1;
-    printf("%d ", idx);
+    cout << idx << " ";
+
     q.push(idx);
+    visit[idx] = true;
+    
     while(1) {
         int num = q.front();
         q.pop();
+
         for(int i = 1; i <= n; i++){
-            if (arr[num][i] == 1 && visit[i] == 0){
-                visit[i] = 1;
-                printf("%d ", i);
+            if(v[num][i] == true && visit[i] == false) {
+                cout << i << " ";
+
+                visit[i] = true;
                 q.push(i);
             }
         }
-        if (q.empty()) break;
+
+        if(q.size() == 0) {
+            break;
+        }
     }
 }
 
 void dfs(int idx) {
-    visit[idx] = 1;
-    printf("%d ", idx);
+    cout << idx << " ";
+
+    visit[idx] = true;
+
     for(int i = 1; i <= n; i++) {
-        if(arr[idx][i] == 1 && visit[i] == 0) dfs(i);
+        if(v[idx][i] == true && visit[i] == false) {
+            dfs(i);
+        }
     }
 }
 
 int main() {
-    scanf("%d %d %d", &n, &m, &v);
-    for(int i = 0; i < m; i++) {
-        scanf("%d %d", &a, &b);
-        arr[a][b] = arr[b][a] = 1;
+    FASTIO
+
+    cin >> n >> m >> k;
+
+    for(int _ = 0; _ < m; _++) {
+        cin >> a >> b;
+
+        v[a][b] = true;
+        v[b][a] = true;
     }
 
-    dfs(v);
-    printf("\n");
-    for(int i = 0; i <= n; i++) visit[i] = 0;
-    bfs(v);
+    dfs(k);
+    cout << "\n";
+
+    for(int i = 0; i <= n; i++) {
+        visit[i] = false;
+    }
+
+    bfs(k);
 }
