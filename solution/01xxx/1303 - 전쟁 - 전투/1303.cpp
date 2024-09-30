97295f2abe904ebb93a24b5dfdd51fe1e61a2959
m1n5eo <skjd1234@naver.com>
2024-09-30 19:50:19 +0900

    Update 1303.cpp

--- solution/01xxx/1303 - 전쟁 - 전투/1303.cpp
+++ solution/01xxx/1303 - 전쟁 - 전투/1303.cpp
@@ -1,14 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int n, m, arr[101][101];
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MAX 111
+
+int n, m;
 char a;
+vector<vector<int>> v(MAX, vector<int>(MAX, 0));
 
 void dfs(int i, int j, int cnt, int k) {
     if(i < 0 || i >= n || j < 0 || j >= m) return;
-    if(arr[i][j] != k) return;
+    if(v[i][j] != k) return;
 
-    arr[i][j] = cnt;
+    v[i][j] = cnt;
 
     dfs(i-1, j, cnt, k);
     dfs(i+1, j, cnt, k);
@@ -17,52 +22,70 @@ void dfs(int i, int j, int cnt, int k) {
 }
 
 int main() {
-    scanf("%d %d", &m, &n); /// m.세로 n.가로
+    FASTIO
+
+    cin >> m >> n;
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            scanf(" %c", &a);
-            if(a == 'W') arr[i][j] = 0;
-            else if(a == 'B') arr[i][j] = 1;
+            cin >> a;
+
+            if(a == 'W') v[i][j] = 0;
+            else if(a == 'B') v[i][j] = 1;
         }
     }
+
     int cnt1 = 2;
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if(arr[i][j] == 0) {
+            if(v[i][j] == 0) {
                 dfs(i, j, cnt1, 0);
-                cnt1++;
+                cnt1 += 1;
             }
         }
     }
+
     int cnt2 = cnt1;
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if(arr[i][j] == 1) {
+            if(v[i][j] == 1) {
                 dfs(i, j, cnt2, 1);
-                cnt2++;
+                cnt2 += 1;
             }
         }
     }
 
     int WhHap = 0, BlHap = 0, WhCnt, BlCnt;
+
     for(int k = 2; k < cnt1; k++) {
         WhCnt = 0;
+        
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(arr[i][j] == k) WhCnt++;
+                if(v[i][j] == k) {
+                    WhCnt += 1;
+                }
             }
         }
+
         WhHap += WhCnt*WhCnt;
     }
+
     for(int k = cnt1; k <= cnt2; k++) {
         BlCnt = 0;
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(arr[i][j] == k) BlCnt++;
+                if(v[i][j] == k) {
+                    BlCnt += 1;
+                }
             }
         }
+
         BlHap += BlCnt*BlCnt;
     }
 
-    printf("%d %d", WhHap, BlHap);
+    cout << WhHap << " " << BlHap;
 }
