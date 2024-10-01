c555775a93d6829b98cbd9145663ca9519045bb0
m1n5eo <skjd1234@naver.com>
2024-10-01 21:23:10 +0900

    Update 20492.cpp

--- solution/20xxx/20492 - 세금/20492.cpp
+++ solution/20xxx/20492 - 세금/20492.cpp
@@ -1,8 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
 int main() {
+    FASTIO
+
     int n;
-    scanf("%d", &n);
-    printf("%d %d", n/100*78, n/1000*956);
+
+    cin >> n;
+    cout << n/100*78 << " " << n/1000*956;
 }
