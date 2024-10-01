553f9c8abf025e7e4e23d9cce35c45caa654b831
m1n5eo <skjd1234@naver.com>
2024-10-01 22:03:36 +0900

    Update 17496.cpp

--- solution/17xxx/17496 - 스타후르츠/17496.cpp
+++ solution/17xxx/17496 - 스타후르츠/17496.cpp
@@ -1,8 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
 int main() {
+    FASTIO
+    
     int n, t, c, p;
-    scanf("%d %d %d %d", &n, &t, &c, &p);
-    printf("%d", (n-1)/t*c*p);
+
+    cin >> n >> t >> c >> p;
+    cout << (n-1)/t*c*p;
 }
