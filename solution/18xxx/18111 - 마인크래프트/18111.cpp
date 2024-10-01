1c392afee0529d51f3c387c28c12f45a9acf4db6
m1n5eo <skjd1234@naver.com>
2024-10-01 21:33:49 +0900

    Update 18111.cpp

--- solution/18xxx/18111 - 마인크래프트/18111.cpp
+++ solution/18xxx/18111 - 마인크래프트/18111.cpp
@@ -1,39 +1,60 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int n, m, b, save, block[255555];
-long long int minTime = 1234567890;
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define lli long long int
+#define INF 1000000000
+#define MAX 255555
 
 bool compare(int a, int b) {
     return a > b;
 }
 
 int main() {
-    scanf("%d %d %d", &n, &m, &b);
-    n = n*m;
-    for(int i = 0; i < n; i++) scanf("%d", &block[i]);
-    sort(block, block+n, compare);
+    FASTIO
+
+    int n, m, b, idx, temp;
+    lli time, result = INF;
+    vector<int> block;
+
+    cin >> n >> m >> b;
+
+    n *= m;
+
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        block.push_back(temp);
+    }
+
+    sort(block.begin(), block.end(), compare);
+
     for(int k = 0; k <= 256; k++) {
-        int temp = b;
-        long long int Time = 0;
+        temp = b;
+        time = 0;
+
         for(int i = 0; i < n; i++) {
             if(block[i] > k) {
-                Time += (block[i]-k)*2;
+                time += (block[i]-k)*2;
                 temp += block[i]-k;
             }
             else if(block[i] < k) {
                 if(temp-(k-block[i]) < 0) {
-                    Time = 1234567890;
+                    time = INF;
                     break;
                 }
-                Time += k-block[i];
+
+                time += k-block[i];
                 temp -= k-block[i];
             }
         }
-        if(Time <= minTime) {
-            minTime = Time;
-            save = k;
+        
+        if(time <= result) {
+            result = time;
+            idx = k;
         }
     }
-    printf("%d %d", minTime, save);
+    
+    cout << result << " " << idx;
 }
