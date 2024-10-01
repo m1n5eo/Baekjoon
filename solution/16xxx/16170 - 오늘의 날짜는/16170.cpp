36bf196aeb76fd753fc927ebb556338ca8e9b208
m1n5eo <skjd1234@naver.com>
2024-10-01 22:10:55 +0900

    Update 16170.cpp

--- solution/16xxx/16170 - 오늘의 날짜는/16170.cpp
+++ solution/16xxx/16170 - 오늘의 날짜는/16170.cpp
@@ -1,8 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
 int main() {
-    printf("2021\n");
-    printf("08\n");
-    printf("06");
+    FASTIO
+    
+    cout << "2024" << "\n";
+    cout << "10" << "\n";
+    cout << "01" << "\n";
 }
