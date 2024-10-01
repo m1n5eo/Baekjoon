27727bd4279bfda8e061a7464bb8bf8b94aa976a
m1n5eo <skjd1234@naver.com>
2024-10-01 22:13:55 +0900

    Update 16430.cpp

--- solution/16xxx/16430 - 제리와 톰/16430.cpp
+++ solution/16xxx/16430 - 제리와 톰/16430.cpp
@@ -1,9 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
 int main() {
+    FASTIO
+    
     int a, b;
-    scanf("%d %d", &a, &b);
-    a = b-a;
-    printf("%d %d", a, b);
+
+    cin >> a >> b;
+    cout << b-a << " " << b;
 }
