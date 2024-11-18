47f36f412cedf57b4fe9ca986fc899e76d61f413
m1n5eo <skjd1234@naver.com>
2024-11-18 18:11:14 +0900

    Update 2922.py

--- solution/02xxx/2922 - 즐거운 단어/2922.py
+++ solution/02xxx/2922 - 즐거운 단어/2922.py
@@ -2,8 +2,6 @@ import sys
 input = sys.stdin.readline
 
 aeiou = ['A', 'E', 'I', 'O', 'U']
-not_aeiou = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
-
 result = 0
 
 def backtracking(index, mo, ja, check, now):
@@ -11,22 +9,17 @@ def backtracking(index, mo, ja, check, now):
 
     if mo >= 3 or ja >= 3:
         return
-
-    if index == len(str):
+    elif index == len(str):
         if check == True:
             result += now
         return
 
     if str[index] == '_':
         backtracking(index+1, mo+1, 0, check, now*len(aeiou))
-        backtracking(index+1, 0, ja+1, check, now*(len(not_aeiou)-1))
-        backtracking(index+1, 0, ja+1, True, now*1)
-    elif str[index] == 'L':
+        backtracking(index+1, 0, ja+1, check, now*(25-len(aeiou)))
         backtracking(index+1, 0, ja+1, True, now)
-    elif str[index] in aeiou:
-        backtracking(index+1, mo+1, 0, check, now)
-    elif str[index] not in aeiou:
-        backtracking(index+1, 0, ja+1, check, now)
+    else:
+        backtracking(index+1, mo+1, 0, check, now) if str[index] in aeiou else backtracking(index+1, 0, ja+1, True if str[index] == 'L' else check, now)
 
 if __name__ == '__main__':
     str = list(input())[:-1]
