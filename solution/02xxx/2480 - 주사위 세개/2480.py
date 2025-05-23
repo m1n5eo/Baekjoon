25a18e91e683eb051d2f433ae10b97e75a436d46
m1n5eo <skjd1234@naver.com>
2025-05-23 16:51:57 +0900

    Create 2480.py

---
+++ solution/02xxx/2480 - 주사위 세개/2480.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+dice = sorted(list(map(int, input().split())))
+
+if dice[0] == dice[2]:
+    print(10000 + dice[1] * 1000)
+elif dice[0] == dice[1] or dice[1] == dice[2]:
+    print(1000 + dice[1] * 100)
+else:
+    print(dice[2] * 100)
