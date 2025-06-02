aa260e1b4b32213307090f39ebf7670c4dc998ab
m1n5eo <skjd1234@naver.com>
2025-06-03 00:18:07 +0900

    Update 19532.py

--- solution/19xxx/19532 - 수학은 비대면강의입니다/19532.py
+++ solution/19xxx/19532 - 수학은 비대면강의입니다/19532.py
@@ -1,10 +1,5 @@
 import sys
 input = sys.stdin.readline
 
-num = list(map(int, input().split()))
-
-for x in range(-999, 999+1):
-    for y in range(-999, 999+1):
-        if num[0]*x + num[1]*y == num[2] and num[3]*x + num[4]*y == num[5]:
-            print(x, y)
-            exit()
+a, b, c, d, e, f = map(int, input().split())
+print((c*e - b*f) // (a*e - b*d), (c*d - a*f) // (b*d - a*e))
