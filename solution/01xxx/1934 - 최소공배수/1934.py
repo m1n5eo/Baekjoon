d2f613159a4aea91d498ae84faca4d474f98efd9
m1n5eo <skjd1234@naver.com>
2025-06-06 11:08:53 +0900

    Create 1934.py

---
+++ solution/01xxx/1934 - 최소공배수/1934.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+def gcd(a, b):
+    return a if b == 0 else gcd(b, a % b)
+
+def lcm(a, b):
+    return (a * b) / gcd(a, b)
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    print(int(lcm(a, b)))
