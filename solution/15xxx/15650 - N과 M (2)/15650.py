9560becf6a0968bfa7efb16648cdbad05cd5477d
m1n5eo <skjd1234@naver.com>
2025-06-11 01:20:31 +0900

    Create 15650.py

---
+++ solution/15xxx/15650 - N과 M (2)/15650.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def f(n, m, result, s):
+    if len(result) == m:
+        for r in result:
+            print(r, end=" ")
+        print()
+        return
+
+    for i in range(s+1, n+1):
+        f(n, m, result + [i], i)
+
+n, m = map(int, input().split())
+f(n, m, [], 0)
