b25ce0c7cc8593f7c9d867a3bdee3e3f90f2da7f
m1n5eo <skjd1234@naver.com>
2025-08-13 23:44:52 +0900

    Create 12852.py

---
+++ solution/12xxx/12852 - 1로 만들기 2/12852.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+MAX = 1111111
+
+n = int(input())
+dp = [[0, 0] for _ in range(n+1)]
+
+for i in range(2, n+1):
+    dp[i] = [dp[i-1][0]+1, i-1]
+    if i%3 == 0 and dp[i//3][0]+1 < dp[i][0]: dp[i] = [dp[i//3][0]+1, i//3]
+    if i%2 == 0 and dp[i//2][0]+1 < dp[i][0]: dp[i] = [dp[i//2][0]+1, i//2]
+
+print(dp[n][0])
+while n != 0:
+    print(n, end=" ")
+    n = dp[n][1]
