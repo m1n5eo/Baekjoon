744eca3f65450df4e4e512bf6b8e5d2ad94ed62d
m1n5eo <skjd1234@naver.com>
2025-09-03 03:35:17 +0900

    Update 2591.cpp

--- solution/02xxx/2591 - 숫자카드/2591.cpp
+++ solution/02xxx/2591 - 숫자카드/2591.cpp
@@ -24,7 +24,6 @@ int solve() {
 		else if(determine > 34) dp[i] = dp[i-1];
 		else dp[i] = dp[i-1] + dp[i-2];
 	}
-	for(int i = 1; i <= n; i++) cout << dp[i] << " "; cout << "\n";
 	return dp[n];
 }
 
