f7a20ce4887bc8461877bf6020a16225ee3350ca
m1n5eo <skjd1234@naver.com>
2025-09-15 03:30:47 +0900

    Create 15311.cpp

---
+++ solution/15xxx/15311 - 약 팔기/15311.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+int main() {
+	FASTIO
+	
+	cout << "2000\n";
+	for(int i = 1; i <= 999; i++) cout << "1 ";
+	for(int i = 1000; i <= 2000; i++) cout << "1000 ";
+}
