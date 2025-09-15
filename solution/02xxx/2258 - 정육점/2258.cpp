cabd630f0aca1c657e6b357c64d85cbc0ea0bebb
m1n5eo <skjd1234@naver.com>
2025-09-15 23:06:14 +0900

    Update 2258.cpp

--- solution/02xxx/2258 - 정육점/2258.cpp
+++ solution/02xxx/2258 - 정육점/2258.cpp
@@ -18,19 +18,12 @@ int solve() {
 	vector<pair<int, int>> buy;
 	sort(meat.begin(), meat.end(), compare);
 	buy.push_back(meat[0]);
-	int total = meat[0].first, sum = 0;
+	int total = meat[0].first;
 	for(int i = 1; i < n; i++) {
-		if(meat[i-1].second != meat[i].second) {
-			sum = total;
-			total += meat[i].first;
-			buy.push_back({total, meat[i].second});
-		}
-		else if(meat[i-1].second == meat[i].second) {
-			total += meat[i].first;
-			buy.push_back({buy[i-1].first+meat[i].first, buy[i-1].second+meat[i].second});
-		}
+        total += meat[i].first;
+		if(meat[i-1].second != meat[i].second) buy.push_back({total, meat[i].second});
+		else if(meat[i-1].second == meat[i].second) buy.push_back({buy[i-1].first+meat[i].first, buy[i-1].second+meat[i].second});
 	}
-	// cout << "\n" << "buy" << "\n"; for(int i = 0; i < n; i++) cout << buy[i].first << " " << buy[i].second << "\n"; cout << "\n";
 	sort(buy.begin(), buy.end(), compare);
 	for(int i = 0; i < n; i++) if(buy[i].first >= m) return buy[i].second;
 	return -1;
