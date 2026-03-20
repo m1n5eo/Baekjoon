9bd0cb1743f249c2ca9aa0127c0540a8e7654a40
m1n5eo <skjd1234@naver.com>
2026-03-21 01:02:37 +0900

    Update 1912.cpp

--- solution/01xxx/1912 - 연속합/1912.cpp
+++ solution/01xxx/1912 - 연속합/1912.cpp
@@ -1,19 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int arr[100003], dp[100003];
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+int solution() {
+    int n;
+    cin >> n;
+    vector<int> array(n), dp(n);
+    for(int i = 0; i < n; i++) {
+        cin >> array[i];
+        if(i == 0) dp[i] = array[i];
+        else dp[i] = max(array[i], dp[i-1] + array[i]);
+    }
+    sort(dp.begin(), dp.end());
+    return dp[n-1];
+}
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-        dp[i] = arr[i];
-    }
-    dp[0] = arr[0];
-    for(int i = 1; i < n; i++) {
-        if(dp[i] < dp[i-1] + arr[i]) dp[i] = dp[i-1] + arr[i];
-    }
-    sort(dp, dp+n);
-    printf("%d", dp[n-1]);
+    FASTIO
+    cout << solution();
 }
