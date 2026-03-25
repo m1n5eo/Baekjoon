5509a169229cd99f2bc5fda99d8e61fe25ec0be5
m1n5eo <skjd1234@naver.com>
2026-03-26 05:01:54 +0900

    Update 20327.cpp

--- solution/20xxx/20327 - 배열 돌리기 6/20327.cpp
+++ solution/20xxx/20327 - 배열 돌리기 6/20327.cpp
@@ -10,17 +10,17 @@ vector<vector<int>> operation(vector<vector<int>> array, int n, int l, int mode)
     for(int x = 0; x < t; x++) {
         for(int y = 0; y < t; y++) {
             int dx = x*k, dy = y*k, ax, ay;
-            if(mode == 5) ax = t-x-1, ay = y;
-            else if(mode == 6) ax = x, ay = t-y-1;
-            else if(mode == 7) ax = t-y-1, ay = x;
-            else if(mode == 8) ax = y, ay = t-x-1;
+            if(mode == 5) ax = (t-x-1)*k, ay = y*k;
+            else if(mode == 6) ax = x*k, ay = (t-y-1)*k;
+            else if(mode == 7) ax = (t-y-1)*k, ay = x*k;
+            else if(mode == 8) ax = y*k, ay = (t-x-1)*k;
             for(int i = 0; i < k; i++) {
                 for(int j = 0; j < k; j++) {
-                    if(mode == 1) ret[i+dx][j+dy] = array[k-i-1+dx][j+dy];
-                    else if(mode == 2) ret[i+dx][j+dy] = array[i+dx][k-j-1+dy];
-                    else if(mode == 3) ret[i+dx][j+dy] = array[k-j-1+dx][i+dy];
-                    else if(mode == 4) ret[i+dx][j+dy] = array[j+dx][k-i-1+dy];
-                    else if(5 <= mode && mode <= 8) ret[i+dx][j+dy] = array[i+ax*k][j+ay*k];
+                    if(mode == 1) ax = dx+k-i-i-1, ay = dy;
+                    else if(mode == 2) ax = dx, ay = dy+k-j-j-1;
+                    else if(mode == 3) ax = dx+k-i-j-1, ay = dy+i-j;
+                    else if(mode == 4) ax = dx-i+j, ay = dy+k-i-j-1;
+                    ret[i+dx][j+dy] = array[i+ax][j+ay];
                 }
             }
         }
